72e3ac73673d8a009121e5ffb8eb8fba07e4b5eb
ritikverma1628 <ritikisverma9000@gmail.com>
2025-10-25 23:49:58 +0530

    Demonstrate ONE TO MANY relationship

---
+++ Relationships/models/customer.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose')
+async function main(){
+    await mongoose.connect('mongodb://127.0.0.1:27017/RelationsDemo');
+    console.log("Connection has been established")
+}
+
+const orderSchema = new mongoose.Schema({
+    item : String, 
+    price : Number
+})
+const Order = mongoose.model('Order', orderSchema);
+
+const customerSchema = new mongoose.Schema({
+    name : String ,
+    orders:[
+        {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : 'Order'
+        }
+    ] 
+
+})
+const Customer = mongoose.model('Customer', customerSchema);
+
+const addCustomer = async()=>{
+    const customer1 = new Customer({name : 'ritik'})
+    const order1 = await Order.findOne({item: 'brownie'});
+    const order2 = await Order.findOne({item: 'chips'});
+
+    customer1.orders.push(order1)
+    customer1.orders.push(order2);
+
+    let result = await customer1.save();
+}
+
+
+const addOrder = async()=>{
+    let result = await Order.insertMany(
+        [{item:'chips', price: 20},
+        {item: 'brownie', price: 50},
+        {item : 'chocolate', price: 100}]
+    )
+    console.log(result);
+}
+
+async function run(){
+    await main();
+    // await addOrder();
+    await addCustomer();
+}
+
+run().catch(err=>console.log(err))
