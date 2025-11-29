c264f48c8b8d8ba9101b6565bbe9abaeceda4271
ritikverma1628 <ritikisverma9000@gmail.com>
2025-11-30 00:13:51 +0530

    adding arrays to props and rendering them

--- react/firstApp/src/Item.jsx
+++ react/firstApp/src/Item.jsx
@@ -1,8 +1,9 @@
-function Item({name, price}){
+function Item({name, price, features}){
   return (
     <>
       <h2>Item name: {name}</h2>
       <p>Item Price: {price}</p>
+      <div>{features.map((feature)=><li>{feature}</li>)}</div>
     </>
   )
 }
--- react/firstApp/src/ItemsTab.jsx
+++ react/firstApp/src/ItemsTab.jsx
@@ -3,9 +3,9 @@ import Item from "./Item";
 function ItemsTab() {
   return (
     <>
-      <Item name={'Laptop'} price={50000}/>
-      <Item name={'Mobile'} price={25000}/>
-      <Item name={'Bottle'} price={500}/>
+      <Item name={'Laptop'} price={50000} features={['fast charging','long battery']}/>
+      <Item name={'Mobile'} price={25000} features={['water proof','gorilla glass']}/>
+      <Item name={'Bottle'} price={500} features={['stainless steel', '1litre']}/>
     </>
   );
 }
