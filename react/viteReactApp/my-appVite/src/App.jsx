1c4007da0bb3e7fe24e71ad80a3c536389584c48
ritikverma1628 <ritikisverma9000@gmail.com>
2025-11-29 17:29:52 +0530

    Getting started with React

--- react/viteReactApp/my-appVite/src/App.jsx
+++
@@ -1,9 +0,0 @@
-
-function App() {
-
-  return (
-    <h1>Ab mai vite ke thorugh aaya hu</h1>
-  )
-}
-
-export default App;
--- react/viteReactApp/my-appVite/src/ritik.jsx
+++
@@ -1,7 +0,0 @@
-function Ritik(){
-    return(
-        <h2>Ab mai yaha dhamal machane aaya hu</h2>
-    );
-}
-
-export default Ritik;
