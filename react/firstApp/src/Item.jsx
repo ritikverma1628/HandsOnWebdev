b8a2119961482b209daf06978aa4d8a96ce1fc18
ritikverma1628 <ritikisverma9000@gmail.com>
2025-11-29 23:45:54 +0530

    Understanding basics

---
+++ react/firstApp/src/Item.jsx
@@ -0,0 +1,11 @@
+function Item(){
+    const itemName = "Laptop";
+  return (
+    <>
+      <h2>Item name: {itemName}</h2>
+      <p>Item Price: {2*10000}</p>
+    </>
+  )
+}
+
+export default Item
---
+++ react/firstApp/src/ItemsTab.jsx
@@ -0,0 +1,13 @@
+import Item from "./Item";
+
+function ItemsTab() {
+  return (
+    <>
+      <Item />
+      <Item />
+      <Item />
+    </>
+  );
+}
+
+export default ItemsTab;
