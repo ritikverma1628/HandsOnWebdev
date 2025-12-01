92db55fdc0a34b692921f99a28123e879c2bcfe6
ritikverma1628 <ritikisverma9000@gmail.com>
2025-12-01 13:28:50 +0530

    Conditionals

--- react/firstApp/src/Item.jsx
+++ react/firstApp/src/Item.jsx
@@ -1,12 +1,15 @@
 import './Item.css'
 
 function Item({name, price, features}){
+  let discountAvaialble=price>30000;
+  let styles={backgroundColor:discountAvaialble ? "lightgreen" : ''}
   return (
     <>
-      <div className='item'>
+      <div className='item'style={styles}>
         <h2>Item name: {name}</h2>
         <p>Item Price: {price}</p>
         <div className="features">{features.map((feature)=><li>{feature}</li>)}</div>
+        {discountAvaialble ? <i>Discount of 5%</i> : null}
       </div>
     </>
   )
