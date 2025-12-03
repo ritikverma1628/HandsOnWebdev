986da64404a6e2f2b1327fbc35a0b6b2a0a9ca40
ritikverma1628 <ritikisverma9000@gmail.com>
2025-12-03 18:46:39 +0530

    using useState() hook to re-render the component after state variable updation

--- react/reactStates/src/Counter.jsx
+++ react/reactStates/src/Counter.jsx
@@ -1,11 +1,13 @@
-export default function Counter(){
-    let count = 0 ;
+import {useState} from 'react'
 
-    function increaseCount(){
-        count = count+1;
-        console.log(count);
-    }
+export default function Counter(){
+    let [count, setCount] = useState(0)  //count is a state variable and can be changed using the setCount function , the component re-renders only after the setCount funciton is invoked
     
+    function increaseCount(){
+        setCount(count+1);
+        console.log(count)
+    }
+
     return(
         <>
             <p onClick={increaseCount}>Count={count}</p>
