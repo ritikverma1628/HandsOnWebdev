ad1d2728442772771607e5d7d9d6433fd5a17890
ritikverma1628 <ritikisverma9000@gmail.com>
2025-12-05 23:20:45 +0530

    Most Basic ToDo App

--- react/todoApp/src/App.jsx
+++ react/todoApp/src/App.jsx
@@ -7,12 +7,6 @@ function App() {
   return (
     <>
       <h1>Create your ToDo List:</h1>
-      <form action="">
-        <input type="text" placeholder='Add task' style={{height:'32px', borderRadius:'10px'}}/> &nbsp;&nbsp;&nbsp;&nbsp;
-        <button>ADD</button>
-      </form>
-      <br /><br />
-      <hr />
       <TodoApp></TodoApp>
     </>
   )
--- react/todoApp/src/TodoApp.jsx
+++ react/todoApp/src/TodoApp.jsx
@@ -1,7 +1,31 @@
+import { useState } from "react"
+
 export default function TodoApp(){
+
+    let [todoTasks, settodoTasks] = useState([])
+    let [task, setTask] = useState('')
+
+    function handletodoTasks(event){
+        event.preventDefault();
+        settodoTasks([...todoTasks, task]);
+        setTask('')
+        
+    }
+
+    function handleTask(event){
+        setTask(event.target.value)
+    }
+
     return(
         <>
-            <p>TodoApp</p>
+            <form action="">
+                <input type="text" placeholder='Add task' value={task} onChange={handleTask} style={{height:'36px', borderRadius:'8px'}}/> &nbsp;&nbsp;&nbsp;&nbsp;
+                <button onClick={handletodoTasks}>ADD</button>
+            </form>
+            <br /><br />
+            <hr />
+            <p>----------ToDo List----------</p>
+            <div>{todoTasks.map((todoTask)=><p>{todoTask}</p>)}</div>
         </>
     )
 }
