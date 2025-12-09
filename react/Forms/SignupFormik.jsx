b1648d5b12d52f09d17eabbcf8b2dd9380ca276e
ritikverma1628 <ritikisverma9000@gmail.com>
2025-12-09 15:34:26 +0530

    Styling the Formik form

--- react/Forms/SignupFormik.jsx
+++ react/Forms/SignupFormik.jsx
@@ -4,33 +4,38 @@ import './SignupFormik.css'
 
 export default function SignupFormik({addComments}){
     return(
-        <Formik 
-        initialValues={{username:'', comment:'', rating:5}} 
-        validationSchema={Yup.object({
-            username:Yup.string().max(15,'username cannot exceed 15 character').required('Username is required'),
-            comment: Yup.string().required('Please fill in some comment'), 
-            rating:Yup.number().min(1, 'Rating should be greater than or equal to 1').max(5, 'Rating should be less than or equal to 5').required()
-        })}
-        onSubmit={(values, {onSubmitting})=>{
-            addComments(values)
-            resetForm();
-        }}>
-            <Form>
-                <label htmlFor="username">Enter your username</label>
-                <Field name='username' type='text'></Field>
-                <ErrorMessage name='username'></ErrorMessage>
+        <div className='form'>
+    
 
-                <label htmlFor="comment">Enter your remarks</label>
-                <Field name='comment' type='text' ></Field>
-                <ErrorMessage name='comment'></ErrorMessage>
+            <h1>Comments Form</h1>
+            <Formik 
+            initialValues={{username:'', comment:'', rating:5}} 
+            validationSchema={Yup.object({
+                username:Yup.string().max(15,'username cannot exceed 15 character').required('Username is required'),
+                comment: Yup.string().required('Please fill in some comment'), 
+                rating:Yup.number().min(1, 'Rating should be greater than or equal to 1').max(5, 'Rating should be less than or equal to 5').required()
+            })}
+            onSubmit={(values, {onSubmitting})=>{
+                addComments(values)
+                resetForm();
+            }}>
+                <Form>
+                    <label htmlFor="username">Enter your username</label><br />
+                    <Field name='username' type='text'></Field><br />
+                    <ErrorMessage name='username'></ErrorMessage><br />
 
-                <label htmlFor="rating">Give some rating</label>
-                <Field name='rating' type='number' ></Field>
-                <ErrorMessage name='rating'></ErrorMessage>
+                    <label htmlFor="comment">Enter your remarks</label><br />
+                    <Field name='comment' type='text' ></Field><br />
+                    <ErrorMessage name='comment'></ErrorMessage><br />
 
-                <button type='submit'>Submit</button>
+                    <label htmlFor="rating">Give some rating</label><br />
+                    <Field name='rating' type='number' ></Field><br />
+                    <ErrorMessage name='rating'></ErrorMessage><br /><br />
 
-            </Form>
-        </Formik>
+                    <button type='submit'>Submit</button><hr /><br />
+
+                </Form>
+            </Formik>
+        </div>
     )
 }
