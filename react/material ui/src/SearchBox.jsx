ebf9f2123052c9960344feedb0d379668b507888
ritikverma1628 <ritikisverma9000@gmail.com>
2025-12-10 18:48:21 +0530

    Fetching coordinates of city using Geocoding API

--- react/material ui/src/SearchBox.jsx
+++ react/material ui/src/SearchBox.jsx
@@ -11,19 +11,26 @@ export default function SearchBox(){
         setCity(e.target.value)
     }
 
-    function handleSubmit(e){
+    async function getGeos(city){
+        let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c6efeb5acfa1357dbcfa11d61c2edb87`)
+        let data = await response.json();
+        return {lon:data.coord.lon, lat:data.coord.lat}
+    }
+
+    async function handleSubmit(e){
         e.preventDefault();
+        let {lon, lat}= await getGeos(city);
         setCity('')
     }
 
     return(
         <>
             <div className="searchbox">
-                <form action="">
+                <form onSubmit={handleSubmit} >
 
                     <label htmlFor="outlined-basic">Enter the city </label><br />
                     <TextField id="outlined-basic" label="City" variant="outlined" value={city} onChange={handleChange}/><br /><br />
-                    <Button type='submit' variant="contained" onSubmit={handleSubmit} >Search</Button>
+                    <Button type='submit' variant="contained" >Search</Button>
 
                 </form>
                 <br /><br />
