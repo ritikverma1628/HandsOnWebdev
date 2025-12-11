27580cbb115e8ad317be6525a9b82231577f85fc
ritikverma1628 <ritikisverma9000@gmail.com>
2025-12-11 23:41:07 +0530

    Rendering weather info taken from parent component as a prop

--- react/material ui/src/WeatherWidget.jsx
+++ react/material ui/src/WeatherWidget.jsx
@@ -2,31 +2,43 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
+import SevereColdIcon from '@mui/icons-material/SevereCold';
+import SunnyIcon from '@mui/icons-material/Sunny';
+import ThunderstormIcon from '@mui/icons-material/Thunderstorm';
 import './WeatherWidget.css'
 
-export default function WeatherWidget(){
+export default function WeatherWidget({info}){
+    let HOT="https://media.istockphoto.com/id/1007768414/photo/blue-sky-with-bright-sun-and-clouds.jpg?s=612x612&w=0&k=20&c=MGd2-v42lNF7Ie6TtsYoKnohdCfOPFSPQt5XOz4uOy4="
+    let COLD="https://media.istockphoto.com/id/868098786/photo/thermometer-on-snow-shows-low-temperatures-zero-low-temperatures-in-degrees-celsius-and.jpg?s=612x612&w=0&k=20&c=jOZH4RSlX29thO6GNlvTUlYKUo_DK4xVxvXUTK7Jw5s="
+    let RAIN="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRut6ERgCLBRRzKbdSvwgtGcI0Dild3W5kwiA&s"
     return(
         <>
-        <div className="weatherwidget">
+        {info.city?<div className="weatherwidget">
 
-            <Card sx={{ maxWidth: 345 }}>
+        <Card sx={{ maxWidth: 345 }} style={{boxShadow: '0px 0px 10px 5px rgb(63, 63, 63)'}}>
             <CardMedia
                 sx={{ height: 190 }}
-                image="https://media.istockphoto.com/id/1007768414/photo/blue-sky-with-bright-sun-and-clouds.jpg?s=612x612&w=0&k=20&c=MGd2-v42lNF7Ie6TtsYoKnohdCfOPFSPQt5XOz4uOy4="
+                image={info.humidity>75?RAIN:(info.temp>15?HOT:COLD)}
                 title="weather"
             />
             <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                Lizard
+                <Typography gutterBottom variant="h4" component="div">
+                {info.city}&nbsp;{info.humidity>75?<ThunderstormIcon/>:(info.temp>15?<SunnyIcon/>:<SevereColdIcon/>)}
                 </Typography>
-                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                Lizards are a widespread group of squamate reptiles, with over 6,000
-                species, ranging across all continents except Antarctica
+                <Typography variant="body2" sx={{ color: 'text.secondary' }} component='div' className='cardContent'>
+                    <div>Temperature = <b>{info.temp}&deg;C</b></div>
+                    <div className="minmaxtemp">
+                        <div>Min. Temperature <b>{info.maxTemp}&deg;C</b></div>
+                        <div>Max. Temperature <b>{info.minTemp}&deg;C</b></div>
+                    </div>
+                    <div>Humidity = <b>{info.humidity}%</b></div>
+                    <div>Pressure = <b>{info.pressure} hPa</b></div>
+                    <div>it feels like <b>{info.feelsLike}&deg;C</b> and its <b>{info.description}</b></div>
                 </Typography>
             </CardContent>
             
             </Card>
-        </div>
+        </div>:null}
 
         </>
     )
