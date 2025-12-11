5dfd328dffd82819178ba297a4af6d03ed1affa9
ritikverma1628 <ritikisverma9000@gmail.com>
2025-12-11 23:39:34 +0530

    Lifting up weather info in parent component

--- react/material ui/src/WeatherWindow.jsx
+++ react/material ui/src/WeatherWindow.jsx
@@ -1,11 +1,27 @@
 import SearchBox from "./SearchBox";
 import WeatherWidget from "./WeatherWidget";
+import { useState } from "react";
 
 export default function (){
+    let [weatherInfo, setWeatherInfo] = useState({})
+
+    function weatherDetails(data){
+        let weatherInfo = {
+            city:data.name, 
+            temp:data.main.temp,
+            maxTemp:data.main.temp_max,
+            minTemp:data.main.temp_min,
+            feelsLike:data.main.feels_like,
+            description:data.weather[0].description, 
+            pressure:data.main.pressure,
+            humidity:data.main.humidity
+        }
+        setWeatherInfo(weatherInfo)
+    }
     return(
         <>
-            <SearchBox></SearchBox>
-            <WeatherWidget></WeatherWidget>
+            <SearchBox weatherDetails={weatherDetails} ></SearchBox>
+            <WeatherWidget info={weatherInfo}></WeatherWidget>
         </>
     )
 }
